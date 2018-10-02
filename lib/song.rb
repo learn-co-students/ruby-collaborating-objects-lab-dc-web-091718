9a4ef384565836f7276a454c0df5a822ce3da0b7
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-10-01 22:29:04 -0400

    donE

--- lib/song.rb
+++ lib/song.rb
@@ -1,66 +1,67 @@
-# class Song
-#   attr_accessor :name, :artist
-#   @@all = []
-#   def initialize(name)
-#     @name = name
-#   end
-#
-#   def self.new_by_filename(filename)
-#    artist, song = filename.split(" - ")
-#    new_song = self.new(song)
-#    new_song.artist_name = artist
-#    new_song.save
-#  end
-#
-#  def artist_name=(name)
-#    self.artist = Artist.find_or_create_by_name(name)
-#    artist.add_song(self)
-#  end
-#
-#  def self.all
-#   @@all
-# end
-#
-#  def save
-#   @@all << self
-#   self
-# end
-# end
-
-
 class Song
   attr_accessor :name, :artist
-
   @@all = []
-
   def initialize(name)
     @name = name
   end
 
   def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = "mickel"
-    new_song
-  end
+   artist, song = filename.split(" - ")
+   new_song = self.new(song)
+   new_song.artist_name = artist
+   new_song.save
+ end
 
-  def self.find_by_artist(artist)
-    Song.all.select do | song |
-      song.artist == artist
-    end
-  end
+ def artist_name=(name)
+   self.artist = Artist.find_or_create_by_name(name)
+   artist.add_song(self)
+ end
 
-  def self.all
-    @@all
-
-  end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-  end
-
-  def save
-    @@all << self
-    self
-  end
+ def self.all
+  @@all
 end
+
+ def save
+  @@all << self
+  self
+end
+end
+
+#
+# class Song
+#   attr_accessor :name, :artist
+#
+#   @@all = []
+#
+#   def initialize(name)
+#     @name = name
+#   end
+#
+#   def self.new_by_filename(filename)
+#     artist, song = filename.split(" - ")
+#     new_song = self.new(song)
+#     new_song.artist_name = artist
+#     Artist.all << new_song
+#     new_song
+#   end
+#
+#   def self.find_by_artist(artist)
+#     Song.all.select do | song |
+#       song.artist == artist
+#     end
+#   end
+#
+#   def self.all
+#     @@all
+#
+#   end
+#
+#   def artist_name=(name)
+#     self.artist = Artist.find_or_create_by_name(name)
+#   end
+#
+#   def save
+#     @@all << self
+#     self
+#   end
+# end
